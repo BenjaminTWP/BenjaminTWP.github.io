a21095c125a71c48d2bc262453880cc06fb2113a
BenjaminTWP <benjamintwpettersson@gmail.com>
2024-06-04 20:15:30 +0200

    I had not added the code

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JavaScript is running!');
     // Add your JavaScript code here
+    // This is where i put stuff I guess
 });

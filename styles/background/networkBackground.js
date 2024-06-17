ed8064d03d9b80b6018881d973fa1e60ca0d630e
BenjaminTWP <benjamintwpettersson@gmail.com>
2024-06-17 19:22:31 +0200

    About me has background visualization and updated font

---
+++ styles/background/networkBackground.js
@@ -0,0 +1,17 @@
+
+VANTA.NET({
+    el: "body",
+    mouseControls: true,
+    touchControls: true,
+    yroControls: false,
+    minHeight: 100,
+    minWidth: 50,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x015249,
+    backgroundColor: 0xfaf9f6,
+    showDots: true,
+    points: 8.00,
+    maxDistance: 18.00,
+    spacing: 20.00
+});

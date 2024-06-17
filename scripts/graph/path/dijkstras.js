eddabcbc8bb0073f9da5434b26e818526c2e71c8
BenjaminTWP <benjamintwpettersson@gmail.com>
2024-06-17 16:23:47 +0200

    Dijkstra's visualization now works as intended

--- scripts/graph/path/dijkstras.js
+++ scripts/graph/path/dijkstras.js
@@ -1,59 +1,74 @@
-async function dijkstras(nodes, edges, startingNode){
-    console.log("Welcome to djikstra's")
+async function dijkstras(nodes, edges, startingNode) {
+    console.log("Welcome to Dijkstra's");
 
-    const select = document.getElementById("graph_visualization_speed");
-    const waitFactor = select.options[select.selectedIndex].value;
-    let distances = {};
     let previousNode = {};
+    let distances = {};
+    let paths = {};
 
-    nodes.forEach(node=>{
+    nodes.forEach(node => {
         distances[node.id] = Infinity;
         previousNode[node.id] = null;
+        paths[node.id] = [];
     });
 
     distances[startingNode] = 0;
     highlightNode(startingNode, COLOR_MARINE);
 
     let priorityQueue = new PriorityQueue();
-    priorityQueue.push([0,startingNode]); //Distance 0 to start node
+    priorityQueue.push([0, startingNode]); // Distance 0 to start node
 
     while (!priorityQueue.is_empty()) {
         let [currentDistance, currentNode] = priorityQueue.pop();
 
         for (const edge of edges) {
-
-            //This 'if' is to because we consider the graph non-directed,
-            //so the node has to come or go from the current node
             if (edge.source.id === currentNode || edge.target.id === currentNode) {
                 let distance = edge.length;
                 let neighbor;
 
-                await wait_for(waitFactor);
-                //highlightNode(currentNode, GREEN);
-
-                if (edge.source.id === currentNode ) {
+                if (edge.source.id === currentNode) {
                     neighbor = edge.target.id;
+                } else {
+                    neighbor = edge.source.id;
                 }
-                else { neighbor = edge.source.id}
 
                 let new_distance = currentDistance + distance;
 
-                if (new_distance < distances[neighbor]){
-
+                if (new_distance < distances[neighbor]) {
                     distances[neighbor] = new_distance;
-                    //highlightNode(neighbor, GREEN);
-                    console.log("from " + ALPHABET[currentNode] + " to " + ALPHABET[neighbor]);
-                    console.log(new_distance);
-                    highlightEdge(currentNode, neighbor, COLOR_MARINE);
-                    highlightEdge(neighbor, currentNode, COLOR_MARINE);
-
-
+                    // Update path to neighbor
+                    paths[neighbor] = paths[currentNode].concat(currentNode);
                     previousNode[neighbor] = currentNode;
                     priorityQueue.push([new_distance, neighbor]);
                 }
             }
         }
-
     }
 
+    await visualizePaths(paths, startingNode);
 }
+
+async function visualizePaths(paths, startingNode) {
+    const select = document.getElementById("graph_visualization_speed");
+    const waitFactor = select.options[select.selectedIndex].value;
+
+    for (let node in paths) {
+
+        if (parseInt(node) !== startingNode) {
+            let path = paths[node];
+            path.push(parseInt(node)); //Pushing the end node makes the following coding easier
+            await wait_for(waitFactor);
+            highlightNode(parseInt(node), COLOR_MARINE);
+            await wait_for(waitFactor);
+
+            for (let i =0; i < paths[node].length - 1; i++){
+                highlightEdge(paths[node][i], paths[node][i+1], COLOR_FEATHER);
+                highlightEdge(paths[node][i+1], paths[node][i] , COLOR_FEATHER);
+                await wait_for(waitFactor);
+                highlightEdge(paths[node][i], paths[node][i+1], COLOR_MARINE);
+                highlightEdge(paths[node][i+1], paths[node][i] , COLOR_MARINE);
+            }
+        }
+    }
+}
+
+

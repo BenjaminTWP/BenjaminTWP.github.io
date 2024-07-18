7ef0e96121bee1ed8c1b21b38320360ba1211153
BenjaminTWP <benjamintwpettersson@gmail.com>
2024-07-18 19:07:14 +0200

    Show user you can not search without having sorted a list

--- scripts/list/listActions.js
+++ scripts/list/listActions.js
@@ -1,27 +1,64 @@
+//* STARTING STATE *//
+
+activateSortButton();
+deactivateSearchButton()
+
+const listInfoIcon = document.getElementById('listInfoIcon');
+const listInfoWindow = document.getElementById('listInfoWindow');
+
+listInfoIcon.addEventListener('mouseenter', function() {
+    listInfoWindow.classList.remove('hidden');
+});
+listInfoIcon.addEventListener('mouseleave', function() {
+    listInfoWindow.classList.add('hidden');
+});
+
 function populateSearchableValues() {
-    let html_selector = document.getElementById("searchForValue");
+    let selectSearchableElement = document.getElementById("searchForValue");
     for (let index = 1; index <= 30; index++) {
         let option = document.createElement("option");
         option.value = index.toString();
         option.textContent = index.toString();
         option.style.textAlign = "center";
-        html_selector.appendChild(option);
+        selectSearchableElement.appendChild(option);
     }
 }
 
 function newBars() {
     let listVisualizer = ListVisualizer.getInstance();
     listVisualizer.newBars();
+    deactivateSearchButton();
 }
 
 async function sortBars() {
     let listVisualizer = ListVisualizer.getInstance();
     listVisualizer.sort();
+    activateSearchButton();
 }
 async function searchBars(){
     let listVisualizer = ListVisualizer.getInstance();
     const searchValue = document.getElementById("searchForValue").value;
     listVisualizer.search(searchValue);
+}
+
+function activateSortButton(){
+    const sortButton = document.getElementById("listSortBarsButton");
+    sortButton.disabled = false;
+    sortButton.style.boxShadow = "0 0 3px #57BC90";
+}
+
+function activateSearchButton(){
+    const searchButton = document.getElementById("listSearchBarsButton");
+    searchButton.disabled = false;
+    searchButton.style.cursor = "pointer";
+    searchButton.style.boxShadow = "0 0 3px #57BC90";
+}
+
+function deactivateSearchButton(){
+    const searchButton = document.getElementById("listSearchBarsButton");
+    searchButton.disabled = true;
+    searchButton.style.cursor = "auto";
+    searchButton.style.boxShadow = "0 0 3px #ff5454";
 }
 
 function getListWaitFactor(){
@@ -32,6 +69,7 @@ function getListWaitFactor(){
 function addBar(){
     const visualizer = ListVisualizer.getInstance();
     visualizer.addBar();
+    deactivateSearchButton()
 }
 
 function removeBar(){
@@ -53,12 +91,3 @@ function setSearchAlgorithm() {
 }
 
 
-const listInfoIcon = document.getElementById('listInfoIcon');
-const listInfoWindow = document.getElementById('listInfoWindow');
-
-listInfoIcon.addEventListener('mouseenter', function() {
-    listInfoWindow.classList.remove('hidden');
-});
-listInfoIcon.addEventListener('mouseleave', function() {
-    listInfoWindow.classList.add('hidden');
-});

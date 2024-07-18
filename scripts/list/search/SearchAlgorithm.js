481c90eeaa996ef2271debf8c3eae956eb6bccb5
BenjaminTWP <benjamintwpettersson@gmail.com>
2024-07-18 18:36:57 +0200

    Search algorithms refactored

--- scripts/list/search/SearchAlgorithm.js
+++ scripts/list/search/SearchAlgorithm.js
@@ -1,9 +1,14 @@
+            //*         *****************************************        //
+            //                   General Search Algorithm                //
+            //          *****************************************       *//
+
+
 class SearchAlgorithm {
     _highLightColor = COLOR_FEATHER;
     _foundColor = COLOR_MARINE;
     _defaultColor = "white";
     _notFoundColor = "black";
-    _barValues;
+    _barElements;
 
     async search(){ throw new Error("Method 'search()' must be implemented.");}
 
@@ -13,24 +18,75 @@ class SearchAlgorithm {
 
     _prepareBarVisualization(){
         const listContainer = document.getElementById("listVisualizer");
-        this._barValues = Array.from(listContainer.children);
+        this._barElements = Array.from(listContainer.children);
         const listVisualizer = ListVisualizer.getInstance();
         listVisualizer.resetBarColors(listContainer);
     }
 
-    _searchValueInArray(bars, searchValue){
-        const low = parseInt(bars[0].getAttribute("integerValue"));
-        const high = parseInt(bars[bars.length - 1].getAttribute("integerValue"));
-        return searchValue >= low && searchValue <= high;
+    _valueInArray(bars, searchValue){
+        const low = this._getIntegerValue(bars[0]);
+        return searchValue >= low;
+    }
+
+    async _searchLeft(bars, searchValue, left, mid) {
+        while (mid >= left) {
+            const midValue = this._getIntegerValue(bars[mid]);
+            if (midValue === parseInt(searchValue)) {
+                await this._markFoundBar(bars[mid]);
+                mid--;
+            } else {break;}
+        }
+    }
+
+    async _searchRight(bars, searchValue, mid, right) {
+        while (mid <= right || mid < (bars.length)) {
+            const midValue = this._getIntegerValue(bars[mid]);
+            if (midValue === parseInt(searchValue)) {
+                await this._markFoundBar(bars[mid]);
+                mid++;
+            } else {break;}
+        }
+    }
+
+    async _highlightBar(bar) {
+        bar.style.backgroundColor = this._highLightColor;
+        await wait(this._waitFactor());
+    }
+
+    async _resetBar(bar) {
+        bar.style.backgroundColor = this._defaultColor;
+        await wait(this._waitFactor());
+    }
+
+    async _markFoundBar(bar) {
+        bar.style.backgroundColor = this._foundColor;
+        await wait(this._waitFactor());
+    }
+
+    async _handleValueNotFound() {
+        this._barElements.forEach(bar => bar.style.backgroundColor = this._notFoundColor);
+        await wait(this._waitFactor() * 4);
+        this._barElements.forEach(bar => bar.style.backgroundColor = this._defaultColor);
+    }
+
+    _getIntegerValue(bar) {
+        return parseInt(bar.getAttribute("integerValue"));
     }
 }
+
+        //*         *****************************************        //
+        //                        Linear Search                      //
+        //          *****************************************       *//
+
 
 class LinearSearch extends SearchAlgorithm{
 
     async search(searchValue) {
         this._prepareBarVisualization();
-        if(this._searchValueInArray(this._barValues, searchValue)){
-            await this.#linear(this._barValues, searchValue);
+        if(this._valueInArray(this._barElements, searchValue)){
+            await this.#linear(this._barElements, searchValue);
+        }
+        else {await this._handleValueNotFound()
         }
     }
 
@@ -39,132 +95,63 @@ class LinearSearch extends SearchAlgorithm{
         let found = false;
 
         while (i < bars.length && !found) {
-            let barValue = parseInt(bars[i].getAttribute("integerValue"));
+            let barValue = this._getIntegerValue(bars[i]);
 
             if (barValue === parseInt(searchValue)) {
                 bars[i].style.backgroundColor = this._foundColor;
-                found = true; // Set found flag to true to stop further iterations
-                await this.#searchRight(bars, searchValue, i, i + 1);
+                found = true;
+                await this._searchRight(bars, searchValue, i, i + 1);
             } else {
                 bars[i].style.backgroundColor = this._highLightColor;
                 await wait(this._waitFactor());
                 bars[i].style.backgroundColor = this._defaultColor;
                 await wait(this._waitFactor());
             }
-
             i++;
         }
-
-        // Continue with other operations after finding the value, if needed
-        // Example: Perform some actions or return some result after search
-        console.log("Search complete or continued operations after finding value");
+        if(!found){await this._handleValueNotFound();}
     }
-
-    async #searchRight(bars, searchValue, mid, right) {
-        while (mid <= right) {
-            const midValue = this.#getIntegerValue(bars[mid]);
-            if (midValue === parseInt(searchValue)) {
-                await this.#markFoundBar(bars[mid]);
-                mid++;
-            } else {
-                break;
-            }
-        }
-    }
-
-    async #markFoundBar(bar) {
-        bar.style.backgroundColor = this._foundColor;
-        await wait(this._waitFactor());
-    }
-    #getIntegerValue(bar) {
-        return parseInt(bar.getAttribute("integerValue"));
-    }
-
 }
+
+        //*         *****************************************        //
+        //                        Binary Search                      //
+        //          *****************************************       *//
 
 
 class BinarySearch extends SearchAlgorithm {
     async search(searchValue) {
         this._prepareBarVisualization();
-        await this.#binarySearch(this._barValues, searchValue, 0, this._barValues.length - 1);
+        await this.#binarySearch(this._barElements, searchValue, 0, this._barElements.length - 1);
     }
 
     async #binarySearch(bars, searchValue, left, right) {
         if (left > right) {
-            await this.#handleValueNotFound();
+            await this._handleValueNotFound();
             return;
         }
 
         const mid = Math.floor((left + right) / 2);
-        const midValue = this.#getIntegerValue(bars[mid]);
+        const midValue = this._getIntegerValue(bars[mid]);
 
-        await this.#highlightBar(bars[mid]);
+        await this._highlightBar(bars[mid]);
 
         if (searchValue < midValue) {
-            await this.#resetBar(bars[mid]);
+            await this._resetBar(bars[mid]);
             await this.#binarySearch(bars, searchValue, left, mid - 1);
         } else if (searchValue > midValue) {
-            await this.#resetBar(bars[mid]);
+            await this._resetBar(bars[mid]);
             await this.#binarySearch(bars, searchValue, mid + 1, right);
         } else {
-            await this.#markFoundBar(bars[mid]);
+            await this._markFoundBar(bars[mid]);
             await this.#searchAdjacent(bars, searchValue, left, mid, right);
         }
     }
 
     async #searchAdjacent(bars, searchValue, left, mid, right) {
-        await this.#searchLeft(bars, searchValue, left, mid - 1);
-        await this.#searchRight(bars, searchValue, mid + 1, right);
+        await this._searchLeft(bars, searchValue, left, mid - 1);
+        await this._searchRight(bars, searchValue, mid + 1, right);
     }
 
-    async #searchLeft(bars, searchValue, left, mid) {
-        while (mid >= left) {
-            const midValue = this.#getIntegerValue(bars[mid]);
-            if (midValue === parseInt(searchValue)) {
-                await this.#markFoundBar(bars[mid]);
-                mid--;
-            } else {
-                break;
-            }
-        }
-    }
-
-    async #searchRight(bars, searchValue, mid, right) {
-        while (mid <= right) {
-            const midValue = this.#getIntegerValue(bars[mid]);
-            if (midValue === parseInt(searchValue)) {
-                await this.#markFoundBar(bars[mid]);
-                mid++;
-            } else {
-                break;
-            }
-        }
-    }
-
-    async #highlightBar(bar) {
-        bar.style.backgroundColor = this._highLightColor;
-        await wait(this._waitFactor());
-    }
-
-    async #resetBar(bar) {
-        bar.style.backgroundColor = this._defaultColor;
-        await wait(this._waitFactor());
-    }
-
-    async #markFoundBar(bar) {
-        bar.style.backgroundColor = this._foundColor;
-        await wait(this._waitFactor());
-    }
-
-    async #handleValueNotFound() {
-        this._barValues.forEach(bar => bar.style.backgroundColor = this._notFoundColor);
-        await wait(this._waitFactor() * 4);
-        this._barValues.forEach(bar => bar.style.backgroundColor = this._defaultColor);
-    }
-
-    #getIntegerValue(bar) {
-        return parseInt(bar.getAttribute("integerValue"));
-    }
 }
 
 
